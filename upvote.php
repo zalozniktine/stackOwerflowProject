0c113ce7a4df19c606425a9d93ba212f222b8c6d
Tine <zalozniktine59@gmail.com>
2021-09-27 19:18:12 +0200

    mal dopolnjen like sistem, vsem odgovorom isto daje

--- upvote.php
+++ upvote.php
@@ -7,15 +7,26 @@ $id = $_POST['answer_id'];
 $l = 1;
 $value=3;
 
+$uporabnik = '';
+$odgovor = '';
+
 $stmt = $pdo->query('SELECT * FROM lajki');
 while ($row = $stmt->fetch())
 {
-    if ($row['Value'] == $l || $row['uporabnik_id'] == $user ||  $row['odgovor_id'] == $id){
+    if ($row['Value'] == $l and $row['uporabnik_id'] == $user and  $row['odgovor_id'] == $id){
         $value = 0;
     }
+    if ($row['Value'] == -1 and $row['uporabnik_id'] == $user and  $row['odgovor_id'] == $id){
+        $value = 1;
+        $uporabnik = $row['uporabnik_id'];
+        $odgovor = $row['odgovor_id'];
+    }
 }
-echo $row;
-if ($value != 0) {
+
+if ($value == 1){
+    $stmt2 = $pdo->prepare('UPDATE lajki SET Value = ? WHERE uporabnik_id=? AND odgovor_id=?');
+    $stmt2->execute([1, $uporabnik, $odgovor]);
+}else if ($value != 0) {
     $query = 'INSERT INTO lajki (Value,uporabnik_id,odgovor_id) VALUES (?,?,?)';
     $pdo->prepare($query)->execute([$l, $user, $id]);
 }else{
